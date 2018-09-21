3d46ee87aac926e2b7bd7d583583cdb5e9ec5ac2
Cedro <cedric1lesueur@gmail.com>
2018-09-21 10:31:48 +0200

    commit

--- src/Controller/ChatonController.php
+++ src/Controller/ChatonController.php
@@ -12,8 +12,11 @@ class ChatonController extends AbstractController
      */
     public function index()
     {
+        $image = 'https://chatfaitdubien.fr/wp-content/uploads/2016/09/chaton-1080x675.jpg';
+
         return $this->render('chaton/index.html.twig', [
             'controller_name' => 'ChatonController',
+            'image_chaton' => $image
         ]);
     }
 
@@ -22,8 +25,11 @@ class ChatonController extends AbstractController
      */
     public function chiot()
     {
+        $image = 'https://static.actu.fr/uploads/2018/06/puppy-1903313-854x569.jpg';
+
         return $this->render('chaton/chiot.html.twig', [
             'controller_name' => 'ChatonController',
+            'image_chiot' => $image
         ]);
     }
 }
